bb972f9d0cc9caf0e538d6a599e1f7a6fb83dcf1
Ted Trivuncic <ted@tedtriv.co.uk>
2025-07-25 15:57:41 +0100

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,39 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace Snake
+{
+    public class Game : IDisposable
+    {
+        public int Width { get; init; }
+        public int Height { get; init; }
+        public string Title { get; init; } = "";
+
+        public Color backgroundColour = new(63, 143, 71);
+        private Vector2 position = new(5, 5);
+
+        internal void Run()
+        {
+            Raylib.InitWindow(Width, Height, Title);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(backgroundColour);
+
+                var rec = new Rectangle(position * 60, 60, 60);
+                Raylib.DrawRectangleRounded(rec, 0.25f, 4, Color.Black);
+
+                Raylib.DrawText($"X: {position.X} Y: {position.Y}", 15, 15, 20, Color.White);
+
+                Raylib.EndDrawing();
+            }
+        }
+
+        void IDisposable.Dispose()
+        {
+            GC.SuppressFinalize(this);
+            Raylib.CloseWindow();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Snake;
+
+class Program
+{
+    public static void Main()
+    {
+        using Game game = new()
+        {
+            Width = 600,
+            Height = 600,
+            Title = "Snake",
+        };
+
+        game.Run();
+    }
+}
